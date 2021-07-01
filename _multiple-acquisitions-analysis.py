84e94d5f75cf58f06bfd0239094255477786b786
giocic2 <74670040+giocic2@users.noreply.github.com>
2021-07-01 18:11:44 +0200

    testing

---
+++ _multiple-acquisitions-analysis.py
@@ -0,0 +1,82 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+filenames = []
+offsetPositions = 0
+offsetStep = 1 # mm
+currentCycle = 0
+
+print('List of *.csv files in the current directory: ')
+with os.scandir(path='.') as directoryScan:
+    for entry in directoryScan:
+            if entry.name.endswith('.csv') and entry.is_file():
+                print(entry.name)
+                
+with os.scandir(path='.') as directoryScan:
+    for entry in directoryScan:
+            if entry.name.endswith('.csv') and entry.is_file():
+                filenames.append(entry.name)
+                print(entry.name)
+            if 'ChA' in entry.name:
+                offsetPositions += 1
+                
+offsetAxis = np.linspace(start = 0, stop = (offsetPositions-1) * offsetStep, num = offsetPositions)
+            
+print('Number of different cone positions: ' + str(offsetPositions))
+
+SAMPLING_FREQUENCY = 10e3 # Hz
+print('Sampling frequency: ' + str(SAMPLING_FREQUENCY) + ' Hz')
+ACQUISITION_TIME = 2 # s
+print('Acquisition time: ' + str(ACQUISITION_TIME) + ' s')
+samplingInterval = 1/SAMPLING_FREQUENCY
+totalSamples = round(ACQUISITION_TIME/samplingInterval)
+FFT_FREQ_BINS = 2**19
+print('FFT resolution: ' + str(SAMPLING_FREQUENCY/FFT_FREQ_BINS) + ' Hz')
+
+FFTpeaks = np.ndarray((2, offsetPositions)) # First row for ChA (IFI), second row for ChB (IFQ)
+
+currentCycle = 0
+
+for entry in filenames:
+    if 'ChA' in entry:
+        rawSamples = np.genfromtxt(entry, delimiter = ',')
+        voltageAxis_mV = rawSamples[:,0]
+        timeAxis_s = rawSamples[:,1]
+        # FFT computation
+        FFT = np.fft.rfft(voltageAxis_mV, n = FFT_FREQ_BINS) # FFT of real signal
+        FFT_mV = np.abs(2/(totalSamples)*FFT) # FFT magnitude
+        FFT_dBV = 20*np.log10(FFT_mV/1000)
+        freqAxis = np.fft.rfftfreq(FFT_FREQ_BINS) # freqBins/2+1
+        freqAxis_Hz = freqAxis * SAMPLING_FREQUENCY
+        centerFrequency = 100
+        argmax_range = 100
+        argmax_startBin = int(round((FFT_FREQ_BINS / (SAMPLING_FREQUENCY) * centerFrequency) - argmax_range / 2))
+        argmax_endBin = int(round((FFT_FREQ_BINS / (SAMPLING_FREQUENCY) * centerFrequency) + argmax_range / 2))
+        FFTpeaks[0, currentCycle] = np.amax(FFT_dBV[argmax_startBin:argmax_endBin])
+        print("{0:.0f} dBV".format(FFTpeaks[0, currentCycle]))
+    if 'ChB' in entry:
+        rawSamples = np.genfromtxt(entry, delimiter = ',')
+        voltageAxis_mV = rawSamples[:,0]
+        timeAxis_s = rawSamples[:,1]
+        # FFT computation
+        FFT = np.fft.rfft(voltageAxis_mV, n = FFT_FREQ_BINS) # FFT of real signal
+        FFT_mV = np.abs(2/(totalSamples)*FFT) # FFT magnitude
+        FFT_dBV = 20*np.log10(FFT_mV/1000)
+        freqAxis = np.fft.rfftfreq(FFT_FREQ_BINS) # freqBins/2+1
+        freqAxis_Hz = freqAxis * SAMPLING_FREQUENCY
+        centerFrequency = 100
+        argmax_range = 100
+        argmax_startBin = int(round((FFT_FREQ_BINS / (SAMPLING_FREQUENCY) * centerFrequency) - argmax_range / 2))
+        argmax_endBin = int(round((FFT_FREQ_BINS / (SAMPLING_FREQUENCY) * centerFrequency) + argmax_range / 2))
+        FFTpeaks[1, currentCycle] = np.amax(FFT_dBV[argmax_startBin:argmax_endBin])
+        print("{0:.0f} dBV".format(FFTpeaks[1, currentCycle]))
+        currentCycle += 1
+
+# IFI and IFQ plots
+plt.plot(offsetAxis, FFTpeaks[0, :], label = 'IFI magnitude @100Hz [dBV]')
+plt.plot(offsetAxis, FFTpeaks[1, :], label = 'IFQ magnitude @100Hz [dBV]')
+plt.xlabel('Target offset [mm]')
+plt.legend()
+plt.grid(True)
+plt.show()
